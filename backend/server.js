ea5f28c0cdeb72d823d7627831762f198d848008
pburatini <pbariquelo11@gmail.com>
2026-06-23 22:04:38 -0300

    arrumei o back de "contato" e de "cadastrar álbum"

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use((req, res, next) => {
+  console.log(`>> ${req.method} ${req.url}`)
+  next()
+})
 app.use("/api", router)
 
 async function start() {
